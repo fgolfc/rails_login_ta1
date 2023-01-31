dfa27f2cda5dda56418125ce024b4ed5d8c6360f
fgolfc <cf.ts.fujimoto@gmail.com>
2023-01-31 21:23:22 +0900

    25.5th commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   private
 
   def login_required
-    redirect_to new_session_path, notice: "ログインしてください" unless logged_in?
+    redirect_to new_session_path unless current_user
+  end
+
+  def logged_in_user
+    unless logged_in?
+      redirect_to login_url
+    end
   end
 end
