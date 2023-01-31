10ad45f5df178b0891e25538e74c14f5516db344
fgolfc <cf.ts.fujimoto@gmail.com>
2023-01-31 21:45:28 +0900

    27th commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   before_action :login_required
+  before_action :flash_alert, unless: :user_signed_in?
 
   private
 
   def login_required
-    redirect_to new_session_path, notice: "ログインしてください" unless logged_in?
+    redirect_to new_session_path unless current_user
+  end
+
+  def logged_in_user
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  def flash_alert
+    flash[:alert] = "ログインしてください" unless user_signed_in?
   end
 end
