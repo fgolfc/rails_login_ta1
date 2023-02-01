22c20999846f80010df99b647edbca9b89c42529
fgolfc <cf.ts.fujimoto@gmail.com>
2023-02-01 17:22:25 +0900

    32th commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_29_054044) do
+ActiveRecord::Schema.define(version: 2023_02_01_045706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
