5dd2d3e0d4f0d03267f2557dbdb5c49b4d95b3e7
fgolfc <cf.ts.fujimoto@gmail.com>
2023-02-03 17:55:37 +0900

    54th commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   get 'login', to:'tasks#index'
-  resource :account, only: [:show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
 end
