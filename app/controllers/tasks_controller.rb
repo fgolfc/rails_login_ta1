8e8c116bff574c2dea5eb6d416755b98e5ecd376
fgolfc <cf.ts.fujimoto@gmail.com>
2023-02-03 18:32:57 +0900

    55th commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :login_required
 
   def index
     @tasks = current_user.tasks
